d06358f91d96652e31a0b19ab51e777433937581
Maksat <106050664+MaxatDzhetenbayev@users.noreply.github.com>
2025-06-24 13:27:48 +0500

    fix: Валидация disciplineId

--- src/queue/dto/create-queue.dto.ts
+++ src/queue/dto/create-queue.dto.ts
@@ -1,9 +1,12 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateQueueDto {
   @ApiProperty({
     description: 'Уникальный идентификатор дисциплины',
     example: 'cmca6vqqz0000l1ngjnls3d8q',
   })
+  @IsString({ message: 'disciplineId должен быть строкой' })
+  @IsNotEmpty({ message: 'disciplineId не должен быть пустым' })
   disciplineId: string;
 }
